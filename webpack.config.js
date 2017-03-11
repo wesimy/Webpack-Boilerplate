248dd40ed53e99b70de9cf4c6558140b2be8a5e2
Moataz <wesimy@gmail.com>
2017-03-11 14:18:58 +0400

    Added AR/EN Fonts

--- webpack.config.js
+++ webpack.config.js
@@ -3,134 +3,108 @@ let webpack = require('webpack');
 let path = require('path');
 let nodeModulesPath = path.join(__dirname, 'node_modules');
 let ExtractTextPlugin = require("extract-text-webpack-plugin");
-//let ex = require("./example/ex-loader");
+let exp = []; // multiple exports array
 
+// Exports configs for each language
+let configs = [
+    {
+        name: 'en',
+        entry: {
+            en: './src/index.en.js'
+        },
+        scssRule: {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [{
+                                loader: "css-loader"
+                            },
+                            {
+                                loader: "rtl-ltr-prefix-loader",
+                                query: {
+                                    dir: 'ltr',
+                                }
+                            },
 
-var enConfig = {
-    context: __dirname,
-    watch: true,
-    entry: {
-        en: ['./src/index.en.js'],
-        //   ar: ['./src/index.ar.js'],
+                            {
+                                loader: "sass-loader"
+                            },
+
+                        ],
+                        fallback: "style-loader"
+                    }),
+                }
     },
-    output: {
-        path: __dirname,
-        filename: "dist/js/app.[name].js"
-    },
-    module: {
-        rules: [{
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "jshint-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }],
-                    fallback: "style-loader"
-                }),
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
+    {
+        name: 'ar',
+        entry: {
+            ar: './src/index.ar.js'
+        },
+        scssRule: {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [{
+                                loader: "rtlcss-loader"
+                            },
+                            {
+                                loader: "rtl-ltr-prefix-loader",
+                                query: {
+                                    dir: 'rtl',
+                                }
+                            },
+
+                            {
+                                loader: "sass-loader"
+                            },
+
+                        ],
+                        fallback: "style-loader"
+                    }),
+                }
+    }
+];
+
+// Generate exports module for each config
+for(let c of configs ){
+        var e = {
+        context: __dirname,
+        watch: true,
+        entry: c.entry,
+        output: {
+            path: __dirname,
+            filename: "dist/js/app.[name].js"
+        },
+        module: {
+            rules: [{
+                    enforce: 'pre',
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "jshint-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [{
                             loader: "css-loader"
-                        },
-                        {
-                            loader: "moataz-loader",
-                            query: {
-                                dir: 'ltr',
-                            }
-                        },
-                        
-                        {
-                            loader: "sass-loader"
-                        },
-                        
-                    ],
-                    fallback: "style-loader"
-                }),
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader?name=dist/fonts/[name].[ext]'
-            }
+                        }],
+                        fallback: "style-loader"
+                    }),
+                },
+                c.scssRule,
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2)$/,
+                    loader: 'file-loader?name=dist/fonts/[name].[ext]'
+                }
+            ]
+        },
+        // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
+        plugins: [
+            new ExtractTextPlugin({
+                publicPath: '../../',
+                filename: 'dist/css/app.[name].css'
+            }),
         ]
-    },
-    // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
-    plugins: [
-        new ExtractTextPlugin({
-            publicPath: '../../',
-            filename: 'dist/css/app.[name].css'
-        }),
-    ]
-};
+    }
+    exp.push(e);
+}
 
-var arConfig = {
-    context: __dirname,
-    watch: true,
-    entry: {
-        // en: ['./src/index.en.js'],
-        ar: ['./src/index.ar.js'],
-    },
-    output: {
-        path: __dirname,
-        filename: "dist/js/app.[name].js"
-    },
-    module: {
-        rules: [{
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "jshint-loader"
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }],
-                    fallback: "style-loader"
-                }),
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        {
-                        loader: "rtlcss-loader"
-                    },
-                    {
-                            loader: "moataz-loader",
-                            query: {
-                                dir: 'rtl',
-                            }
-                        },
-                    {
-                        loader: "sass-loader"
-                    },
-                    ],
-                    fallback: "style-loader"
-                }),
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)$/,
-                loader: 'file-loader?name=dist/fonts/[name].[ext]'
-            }
-        ]
-    },
-    // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
-    plugins: [
-        new ExtractTextPlugin({
-            publicPath: './',
-            filename: 'dist/css/app.[name].css'
-        }),
-    ]
-};
-
-
-module.exports = [enConfig, arConfig];
+module.exports = exp;
